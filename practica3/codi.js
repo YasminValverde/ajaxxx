6b07921db050e37281e5055c4e6109a50ef8138d
YasminValverde <yasminvalverde5@gmail.com>
2026-05-08 17:17:10 +0200

    cambios

--- practica3/codi.js
+++ practica3/codi.js
@@ -1,5 +1,20 @@
 
 window.onload = function () {
-
-
+var link = document.querySelector('a');
+for (let i = 0; i < link.length; i++) {
+   link[i].;
+    
+}
+function enviar(e) {
+    e.preventDefault();
+    var peticio = new XMLHttpRequest();
+    peticio.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        document.getElementById("resultat").innerHTML = this.responseText;
+    }
+    };
+    peticio.open("GET", this.href, true);
+    peticio.send(); 
+    
+}
 };
